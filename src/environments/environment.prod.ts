ed0f3afd81805e5f40dc1c61f34fbe94314eda04
chegior <chegiorca@hotmail.com>
2020-07-10 12:41:50 -0500

    Added FIREBASE to enviorements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase:{
+    apiKey: "AIzaSyBq7iHxVUELe_wbo1mO6fiKcL45dzSKrdg",
+    authDomain: "clientpanelprod-d0bba.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-d0bba.firebaseio.com",
+    projectId: "clientpanelprod-d0bba",
+    storageBucket: "clientpanelprod-d0bba.appspot.com",
+    messagingSenderId: "381763771821",
+    appId: "1:381763771821:web:8554cb7ad5521dea0e9524",
+    measurementId: "G-X2V91HE8X6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBq7iHxVUELe_wbo1mO6fiKcL45dzSKrdg",
+    authDomain: "clientpanelprod-d0bba.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-d0bba.firebaseio.com",
+    projectId: "clientpanelprod-d0bba",
+    storageBucket: "clientpanelprod-d0bba.appspot.com",
+    messagingSenderId: "381763771821",
+    appId: "1:381763771821:web:8554cb7ad5521dea0e9524",
+    measurementId: "G-X2V91HE8X6"
+  }
 };
 
 /*
