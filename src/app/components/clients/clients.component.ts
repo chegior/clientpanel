bb1e5c734155339d0a4411f30b0052032eceb3a5
chegior <chegiorca@hotmail.com>
2020-07-11 12:23:05 -0500

    Clients are being fetch from FireBase into app and show in the view

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from 'src/app/models/client';
 
 @Component({
   selector: 'app-clients',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
-  constructor() { }
+  clients:Client[];
+  totalOwed: number;
+
+  constructor(private clientService: ClientService) { }
 
   ngOnInit() {
+    this.clientService.getClients().subscribe(val=>{
+      this.clients = val;
+     this.getTotalOwed();
+    })
+  }
+
+  getTotalOwed(){
+    this.totalOwed = this.clients.reduce((total,client)=>{
+      return total + client.balance;
+    },0);
+    
   }
 
 }
