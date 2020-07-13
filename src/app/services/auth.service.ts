408efa5e22493dc470b316f4e72b24fd8e883dab
chegior <chegiorca@hotmail.com>
2020-07-13 15:25:09 -0500

    adding all the functionality of the app '

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private afsAuth: AngularFireAuth
+  ) { }
+
+  login(email: string, password:string){
+    return new Promise((resolve,reject)=>{
+      this.afsAuth.auth.signInWithEmailAndPassword(email, password)
+        .then(userData => resolve(userData),
+        err => reject(err))
+    });
+  }
+  getAuth(){
+    return this.afsAuth.authState.pipe(auth => auth);
+  }
+
+  logout(){
+    this.afsAuth.auth.signOut();
+  }
+}
